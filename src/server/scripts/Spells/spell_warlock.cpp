96e4cb6269f93d51dab1a2c3d61e4ceb021dd0db
quadral <airhawk@hotmail.co.uk>
2017-05-13 19:12:58 +0100

    fix build error

--- src/server/scripts/Spells/spell_warlock.cpp
+++ src/server/scripts/Spells/spell_warlock.cpp
@@ -2219,10 +2219,11 @@ class spell_warl_nightfall : public SpellScriptLoader
 				if (Player* player = GetCaster()->ToPlayer())
 				{
 					if (player->GetActiveSpec() == SPEC_WARLOCK_AFFLICTION)
-					{ 
-					if (roll_chance_i(5))
-						player->CastSpell(player, WARLOCK_NIGHTFALL);
 					{
+						if (roll_chance_i(5))
+							player->CastSpell(player, WARLOCK_NIGHTFALL);
+					}
+
 				}
             }
 
