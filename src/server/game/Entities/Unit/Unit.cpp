38b11f2952d4ed2c3b74256ad644091bd74b4900
Markus <loewem88@gmail.com>
2017-06-18 13:31:22 -0500

    Revert "Revert "Core/Battle Fatigue: Battle Fatigue Should affect when get hit from other player""
    This reverts commit b75fbb4b017f9e0d2f63acaa16e75bc35c63d85d.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13667,6 +13667,10 @@ void Unit::EnergizeBySpell(Unit* victim, uint32 spellID, int32 damage, Powers po
 
 uint32 Unit::SpellDamageBonusDone(Unit* victim, SpellInfo const* spellProto, uint32 pdamage, DamageEffectType damagetype, uint32 stack)
 {
+
+	if (victim && victim->GetTypeId() == TYPEID_PLAYER && GetTypeId() == TYPEID_PLAYER && !victim->HasAura(134735))
+		if (SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(134735))
+			Aura::TryRefreshStackOrCreate(spellInfo, MAX_EFFECT_MASK, victim, victim);
     if (!spellProto || !victim || damagetype == DIRECT_DAMAGE)
         return pdamage;
 
@@ -15451,6 +15455,10 @@ bool Unit::IsImmunedToSpellEffect(SpellInfo const* spellInfo, uint32 index, bool
 
 uint32 Unit::MeleeDamageBonusDone(Unit* victim, uint32 pdamage, WeaponAttackType attType, SpellInfo const* spellProto)
 {
+	if (victim && victim->GetTypeId() == TYPEID_PLAYER && GetTypeId() == TYPEID_PLAYER && !victim->HasAura(134735))
+		if (SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(134735))
+			Aura::TryRefreshStackOrCreate(spellInfo, MAX_EFFECT_MASK, victim, victim);
+
     if (!victim || pdamage == 0)
         return 0;
 
