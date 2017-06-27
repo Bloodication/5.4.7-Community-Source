a27d6eba6f167051d5524e04c4c0867673b8ed80
Emirate <jordan83222@gmail.com>
2017-06-27 11:08:30 -0400

    Tabard Vendor

---
+++ sql/world/Tabard_NPC.sql
@@ -0,0 +1,24 @@
+--[[Tabard Vendor]]--
+--I CBA doing all of that stuff. If you want to look at the NPC, go for it.
+
+
+
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 98162, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 103636, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 101697, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 103636, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 52252, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 45983, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 49054, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 23705, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 38309, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 38310, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 38311, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 38312, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 38313, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 38314, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 40643, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 43300, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 43348, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 43349, 0, 0, 0, 1);	
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES (989990, 0, 23709, 0, 0, 0, 1);	
