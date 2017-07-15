ade00f68ceee8c3e3b68a360dd5c93a03cc1ca1e
Nyr <pedrorta97@gmail.com>
2017-07-15 06:00:38 +0000

    spell_shaman.cpp -> lava surge proc while casting lava burst fix

--- src/server/scripts/Spells/spell_shaman.cpp
+++ src/server/scripts/Spells/spell_shaman.cpp
@@ -2405,9 +2405,24 @@ class spell_sha_lava_burst : public SpellScriptLoader
 
                 SetHitDamage(GetHitDamage() * 1.5f); // If Flame Shock is on the target, Lava Burst will deal 50% additional damage
             }
+            
+            /// Fix for Lava Surge if proc while player is casting - 77762
+             void HandleAfterCast()
+            {
+                Player* _Caster = GetCaster()->ToPlayer();
+
+                if (_Caster == nullptr)
+                    return;
+
+                if (_Caster->HasAura(SPELL_SHA_LAVA_SURGE))
+                {
+                    if (_Caster->HasSpellCooldown(SPELL_SHA_LAVA_BURST))
+                        _Caster->RemoveSpellCooldown(SPELL_SHA_LAVA_BURST, true);
+            }
 
             void Register()
             {
+                AfterCast += SpellCastFn(spell_sha_lava_burst_SpellScript::HandleAfterCast);
                 OnEffectHitTarget += SpellEffectFn(spell_sha_lava_burst_SpellScript::HandleOnHit, EFFECT_0, SPELL_EFFECT_SCHOOL_DAMAGE);
                 AfterHit += SpellHitFn(spell_sha_lava_burst_SpellScript::HandleAfterHit);
             }
