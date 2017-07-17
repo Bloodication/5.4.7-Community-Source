305a6c31a0fd7b5f079f1cd0c3c5bb90d59bada5
Emirate <jordan83222@gmail.com>
2017-07-17 16:08:26 -0400

    db changes to all fixed spells

---
+++ sql/world/spell_fixes.sql
@@ -0,0 +1,9 @@
+--[[DB Changes]]--
+
+1-3 = spell_script_names
+1. 114635 - spell_warl_ember_tap_glyph
+2. 111264 - spell_mage_ice_ward
+3. 112833 - remove
+4. Creature_Template - 54984: Attack Power -> 35000
+
+Just doing this because I'm too lazy to do it correctly
