99cc131b7288920c7205001f3ca6ed559d0585ca
Emirate <jordan83222@gmail.com>
2017-07-17 16:17:38 -0400

    woops too strong, nerf it back a bit

--- sql/world/spell_fixes.sql
+++ sql/world/spell_fixes.sql
@@ -4,6 +4,6 @@
 1. 114635 - spell_warl_ember_tap_glyph
 2. 111264 - spell_mage_ice_ward
 3. 112833 - remove
-4. Creature_Template - 54984: Attack Power -> 35000
+4. Creature_Template - 54984: Attack Power -> 20000
 
 Just doing this because I'm too lazy to do it correctly
